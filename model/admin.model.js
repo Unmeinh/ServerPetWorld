202a050ee41d6629d2a6e94c76c5edf837add945
DaoMinhHoan224 <daohoan983@gmail.com>
2023-08-09 22:26:16 +0700

    check

--- model/admin.model.js
+++ model/admin.model.js
@@ -1,19 +1,18 @@
 let db = require('./db');
 let AdminSchema = new db.mongoose.Schema(
     {
-        nameAdmin:{type:String,required:true},
-        usernameAdmin:{type:String,required:true},
-        passwordAdmin:{type:String,required:true},
+        fullName:{type:String,required:true},
+        userName:{type:String,required:true,index:{unique:true}},
+        email:{type:String,required:true,index:{unique:true}},
+        passWord:{type:String,required:true},
         avatarAdmin:{type:String,required:false},
-        statusAdmin:{type:String,require:true},
-        emailAdmin:{type:String,required:true,index:{
-            unique:true
-        }}
+        status:{type:String,require:false},
     },
     {
         collection:'Admin'
     }
 )
+
 let AdminModel = db.mongoose.model('AdminModel',AdminSchema);
 
 module.exports={AdminModel};
--- model/user.model.js
+++ model/user.model.js
@@ -1,31 +1,57 @@
 let db = require('./db');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+const string_word_secret = process.env.TOKEN_SEC_KEY
 let UserSchema = new db.mongoose.Schema(
     {
-        usernameUser:{type:String,required:true},
-        passwordUser:{type:String,required:true},
-        fullname:{type:String,required:true},
-        emailUser:{type:String,required:true,index:{
-            unique:true
-        }},
-        // emailUser:{type:String,required:true},
-        phoneNumber:{type:Number,required:true},
-        birthdayUser:{type:Date,required:false},
-        locationUser:{type:Array,required:false},
-        avatarUser:{type:Array,required:false},
+        userName:{type:String,required:true,index:{unique:true}},
+        passWord:{type:String,required:true},
+        fullName:{type:String,required:false},
+        email:{type:String,required:false},
+        phoneNumber:{type:Number,required:true,index:{unique:true}},
+        birthday:{type:Date,required:false},
+        locationUser:{type:String,required:false},
+        locationDelivery:{type:Array,required:false},
+        avatarUser:{type:String,required:false},
         description:{type:String,required:false},
-        genderUser:{type:String,required:false},
-        statusUser:{type:String,required:false},
-        nicknameUser:{type:String,required:false},
+        status:{type:String,required:false},
+        nickName:{type:String,required:false},
         followers:{type:Array,required:false},
         following:{type:Array,required:false},
         createAt:{type:Date,required:false},
-        myPet:{type:Array,required:false}
-        
+        myPet:{type:Array,required:false},
+        token:{type:String,required:true}
     },
     {
         collection:'User'
     }
 )
+
+UserSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    console.log("user "+user);
+    const token = jwt.sign({ _id: user._id, userName: user.userName }, string_word_secret);
+    user.token = token;
+    await user.save();
+    return token;
+}
+
+UserSchema.statics.findByCredentials = async(userName,passWord)=>{
+    const user = await UserModel.findOne({userName});
+    if(!user)
+    {
+        throw new Error({error:'Không tồn tại user này'});
+    }
+    const isPasswordMatch = await bcrypt.compare(passWord, user.passWord)
+   if (!isPasswordMatch) {
+       throw new Error({error: 'Sai password rồi'});
+   }
+   return user;
+
+}
+
+
 let UserModel = db.mongoose.model('UserModel',UserSchema);
 
 module.exports={UserModel};
