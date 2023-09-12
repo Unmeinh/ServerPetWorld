8a02dc1fb68b7de79f312d00c68ce1658cd83cb0
TYNHI <134083525+tynhi27211@users.noreply.github.com>
2023-09-12 23:27:55 +0700

    Update userApi.js

--- routes/api/userApi.js
+++ routes/api/userApi.js
@@ -3,16 +3,21 @@ var UserApiCtrl = require('../../controller/api/user.api');
 var router = express.Router();
 var mdJWT= require('../../middlewares/api.auth');
 
-router.get('/list/all',mdJWT.api_auth, UserApiCtrl.listUser);
-router.get('/myDetail',mdJWT.api_auth, UserApiCtrl.myDetail);
-// router.get('/autoLogin', UserApiCtrl.autoLogin);
-router.get('/userDetail/:idUser',mdJWT.api_auth, UserApiCtrl.detailUser);
-router.post('/register', UserApiCtrl.registUser);
+router.get('/list/all', UserApiCtrl.listUser);
+router.get('/myDetail', mdJWT.api_auth, UserApiCtrl.myDetail);
+router.get('/autoLogin', mdJWT.api_auth, UserApiCtrl.autoLogin);
+router.get('/userDetail/:idUser', mdJWT.api_auth, UserApiCtrl.detailUser);
+router.post('/register', UserApiCtrl.registerUser);
 router.post('/login', UserApiCtrl.loginUser);
 /** bổ sung phần quên mạt khẩu sau */ 
 router.get('/logout', UserApiCtrl.logoutUser);
-router.put('/update/:idUser',mdJWT.api_auth, UserApiCtrl.editUser);
-router.delete('/delete',mdJWT.api_auth,UserApiCtrl.deleteUser);
+router.put('/update/:idUser', mdJWT.api_auth, UserApiCtrl.editUser);
+router.put('/updatePassword', mdJWT.api_auth, UserApiCtrl.updatePassword);
+router.put('/changePassword', UserApiCtrl.changePassword);
+router.delete('/delete/:idUser', UserApiCtrl.deleteUser);
+router.post('/sendVerifyEmail', UserApiCtrl.sendVerifyEmail);
+router.post('/sendResetPasswordEmail', UserApiCtrl.sendResetPassword);
+router.post('/verifyResetPasswordCode', UserApiCtrl.verifyResetCode);
 
 // router.post('/list/following/:idUser');
 // router.post('/list/follower/:idUser');
