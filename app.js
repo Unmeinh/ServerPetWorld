9d8e9c167ae6440a7dd290ba84fc4d09517d29ba
Unmeinh <khongphaihoanglinh@gmail.com>
2023-09-12 22:26:02 +0700

    Push 12/09

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/function', express.static('function'))
 
 app.use(session({
   secret: process.env.KEY_LOGIN_ADMIN,
