17b69b4cea2ecc14ca18d0fcbf279bda8c8b6cea
nguyenvanquyet0710 <quyetvn13@gmail.com>
2023-09-21 23:30:18 +0700

    [billProduct]Api update

--- controller/api/billProduct.api.js
+++ controller/api/billProduct.api.js
@@ -1,144 +1,148 @@
-let mdBlog = require('../../model/blog.model');
-let fs = require('fs');
-exports.listAllBlog = async (req, res, next) => {
-
+const { post } = require("../../app");
+let mdbillProduct = require("../../model/billProduct.model");
+let mdCart = require("../../model/cart.model");
+exports.listbillProduct = async (req, res, next) => {
+  try {
+    // console.log("req: "+req.user);
+    let listbillProduct = await mdbillProduct.billProductModel.find().populate('products.idProduct');
+    if (listbillProduct) {
+      return res.status(200).json({
+        success: true,
+        data: listbillProduct,
+        message: "Lấy danh sách hóa đơn thành công",
+      });
+    } else {
+      return res.status(203).json({
+        success: false,
+        data: [],
+        message: "Không có dữ hóa đơn",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+exports.billProductUser = async (req, res, next) => {
+  console.log(req.body)
+  const { _id } = req.user;
+  const { products: { idProduct, amount, price, discount }, location, total, paymentMethods, deliveryStatus, discountBill, type } = req.body;
+  if (req.method == "POST") {
+    let newbillProduct = new mdbillProduct.billProductModel();
+    newbillProduct.idUser = _id;
+    newbillProduct.location = location;
+    newbillProduct.total = total;
+    newbillProduct.paymentMethods = paymentMethods;
+    newbillProduct.purchaseDate = new Date();
+    newbillProduct.deliveryStatus = deliveryStatus;
+    newbillProduct.discountBill = discountBill;
+    if (type == 1) {
+      let listCartUser = await mdCart.CartModel.findOne({ idUser: _id }).populate("carts.idProduct");
+      let billProduct = [];
+      listCartUser.carts.map((item) => {
+        if (item.isSelected) {
+          const product = {
+            idProduct: item.idProduct._id,
+            amount: item.amount,
+            price: item.idProduct.priceProduct,
+            discount: item.idProduct.discount
+          }
+          billProduct.push(product);
+        }
+      })
+      newbillProduct.products = billProduct;
+    } else if (type == 0) {
+      const itembillProduct = {
+        idProduct,
+        amount,
+        price,
+        discount
+      };
+      newbillProduct.products = [itembillProduct];
+    }
     try {
-        let listAllBlog = await mdBlog.BlogModel.find().populate('idUser').sort({createdAt:-1});
-        if (listAllBlog) {
-            return res.status(200).json({ success: true, data: listAllBlog, message: "Lấy danh sách tất cả bài viết thành công" });
-        }
-        else {
-            return res.status(203).json({ success: false, message: "Không có dữ liệu blog" });
-        }
-
+      const data = await newbillProduct.save();
+      res.status(201).json({ success: true, message: "tạo hóa đơn thành công!", data: data })
     } catch (error) {
-        return res.status(500).json({ success: false, message: error.message });
+      if (error.message.match(new RegExp('.+`location` is require+.'))) {
+        msg = 'Địa chỉ đang trống!';
+      }
+      else if (error.message.match(new RegExp('.+`total` is require+.'))) {
+        msg = 'Tổng tiền đang trống!';
+      }
+      if (typeof newbillProduct.total != 'number' || isNaN(newbillProduct.total) || newbillProduct.total <= 0) {
+        msg = 'Tổng tiền phải nhập số dương!';
+      }
+      else if (error.message.match(new RegExp('.+`paymentMethods` is require+.'))) {
+        msg = ' phương thức thanh toán đang trống!';
+      }
+      else if (error.message.match(new RegExp('.+`deliveryStatus` is require+.'))) {
+        msg = 'Trạng thái giao hàng đang trống!';
+      }
+      else if (error.message.match(new RegExp('.+`discountBill` is require+.'))) {
+        msg = 'Tổng số lượng hóa đơn đang trống!';
+      }
+      else if (isNaN(newbillProduct.discountBill) || newbillProduct.discountBill <= 0) {
+        msg = 'Tổng số lượng hóa đơn phải nhập số!';
+      }
+      else if (error.message.match(new RegExp('.+`idProduct` is require+.'))) {
+        msg = 'Id sản phẩm đang trống!';
+      }
+      else if (error.message.match(new RegExp('.+`amount` is require+.'))) {
+        msg = 'Số lượng sản phẩm đang trống!';
+      }
+      else if (isNaN(newbillProduct.amount) || newbillProduct.amount <= 0) {
+        msg = 'Số lượng sản phẩm phải nhập số!';
+        return res.status(500).json({ success: false, data: {}, message: msg });
+      }
+      else if (error.message.match(new RegExp('.+`discount` is require+.'))) {
+        msg = 'Tổng Số lượng sản phẩm đang trống!';
+      }
+      else if (isNaN(newbillProduct.discount) || newbillProduct.discount <= 0) {
+        msg = 'Tổng Số lượng sản phẩm phải nhập số!';
+      }
+      else {
+        msg = error.message;
+      }
+      return res.status(500).json({ success: false, message: error.message });
     }
-}
-exports.listBlogFromIdUser = async (req, res, next) => {
-    let idUser = req.params.idUser;
-    try {
-        let listBlogUser = await mdBlog.BlogModel.find({ idUser: idUser }).populate('idUser');
-        if (listBlogUser) {
-            return res.status(200).json({ success: true, data: listBlogUser, message: "Lấy danh sách bài viết thành công" });
-        }
-        else {
-            return res.status(203).json({ success: false, data: [], message: "Không có dữ liệu blog" });
-        }
+  }
+  };
 
-    } catch (error) {
-        return res.status(500).json({ success: false, message:'Lỗi: ' +error.message });
-    }
-}
-exports.detailBlog = async (req, res, next) => {
-    let idBlog = req.params.idBlog;
-    try {
-        let objBlog = await mdBlog.BlogModel.findById(idBlog);
-        return res.status(200).json({ success: true, data: objBlog, message: "Lấy dữ liệu blog thành công" });
-    } catch (error) {
-        return res.status(500).json({ success: false, data: {}, message: "Lỗi: " + error.message });
-    }
-}
+  exports.editbillProduct = async (req, res, next) => {
+    // const { _id } = req.user;
+    // const { idProduct, action } = req.body;
+    // if (req.method == "POST") {
+    //   try {
+    //     let listbillProductUser = await mdbillProduct.billProductModel.findOne({ idUser: _id }).populate('billProducts.idProduct');
+    //     listbillProductUser.billProducts.map((item, index) => {
+    //       if (item.idProduct == idProduct) {
+    //         switch (action) {
+    //           case "Increment":
+    //             return (item.amount += 1);
+    //           case "Decrement":
+    //               if(item.amount > 1){
+    //                   return (item.amount -= 1);
+    //               }else{
+    //                   listbillProductUser.billProducts.splice(index, 1);
+    //               }
+    //           default:
+    //             return item;
+    //         }
+    //       }
+    //     });
+    //     await mdbillProduct.billProductModel.findByIdAndUpdate(
+    //       { _id: listbillProductUser._id },
+    //       listbillProductUser
+    //     );
+    //     return res.status(200).json({
+    //       success: true,
+    //       data: listbillProductUser,
+    //       message: "Cật nhật giỏ hàng thành công",
+    //     });
+    //   } catch (error) {
+    //     console.log(error.message);
+    //     return res.status(500).json({ success: false, message: error.message });
 
-exports.addBlog = async (req, res, next) => {
-    console.log('req... '+req.method);
-    if (req.method == 'POST') {
-        console.log('==========');
-        try {
-            let newBlog = new mdBlog.BlogModel();
-            newBlog.contentBlog = req.body.contentBlog;
-            newBlog.contentFont = req.body.contentFont;
-
-            if (req.files != undefined) {
-                console.log("check "+req.files);
-                req.files.map((file, index, arr) => {
-                    console.log("check2 "+file);
-                  if (file != {}) {
-                    fs.renameSync(file.path, './public/upload/' + file.originalname);
-                    let imagePath = 'http://localhost:3000/upload/' + file.originalname;
-                    newBlog.imageBlogs.push(imagePath);
-                  }
-                })
-              }
-           
-            newBlog.aspectRatio = req.body.aspectRatio;
-            newBlog.idUser = req.body.idUser;
-            newBlog.createdAt = new Date();
-            newBlog.comments=0;
-            newBlog.shares=0;
-
-            await newBlog.save();
-            return res.status(201).json({ success: true, data: newBlog, message: "Đã đăng bài viết mới" });
-        } catch (error) {
-            console.log(error.message);
-            let message = '';
-            if (error.message.match(new RegExp('.+`contentBlog` is require+.'))) {
-                message = 'Bạn chưa nhập nội dung!';
-            }
-            else {
-                message = "Đăng bài viết thất bại " + error.message;
-            }
-            return res.status(500).json({ success: false, data: {}, message: message });
-
-        }
-    }
-}
-exports.editBlog = async (req, res, next) => {
-   
-    let idBlog = req.params.idBlog;
-    if (req.method == 'PUT') {
-        try {
-           let newBlog = new mdBlog.BlogModel();
-            newBlog.contentBlog = req.body.contentBlog;
-            newBlog.contentFont = req.body.contentFont;
-
-            if (req.files != undefined) {
-                req.files.map((file, index, arr) => {
-                  if (file != {}) {
-                    fs.renameSync(file.path, '../../public/upload/' + file.originalname);
-                    let imagePath = 'http://localhost:3000/upload/' + file.originalname;
-                    newBlog.imageBlogs.push(imagePath);
-                  }
-                })
-              }
-
-            // if (req.files && req.files.length > 0) {
-            //      newBlog.imageBlogs = req.files.map(file => "http://localhost:3000/upload/"  + file.originalname); // Lưu đường link + tên tệp vào mảng
-            // } else {
-            //     newBlog.imageBlogs = []; // Mảng rỗng nếu không có ảnh được tải lên
-            // }
-            newBlog.imageBlogs = req.body.imageBlogs;
-            newBlog.aspectRatio = req.body.aspectRatio;
-            newBlog.idUser = req.body.idUser;
-            newBlog.createdAt = new Date();
-            newBlog.comments=0;
-            newBlog.shares=0;
-
-            await mdBlog.BlogModel.findByIdAndUpdate(idBlog, newBlog);
-            return res.status(200).json({ success: true, data: newBlog, message: "Đã sửa bài viết" });
-        } catch (error) {
-            console.log(error.message);
-            let message = '';
-            if (error.message.match(new RegExp('.+`contentBlog` is require+.'))) {
-                message = 'Bạn chưa nhập nội dung!';
-            }
-            else {
-                message = "Sửa bài viết thất bại";
-                console.log(error.message);
-            }
-            return res.status(500).json({ success: false, data: {}, message: message });
-
-        }
-    }
-}
-exports.deleteBlog = async (req, res, next) => {
-    let idBlog = req.params.idBlog;
-    if (req.method == 'DELETE') {
-        try {
-            await mdBlog.BlogModel.findByIdAndDelete(idBlog);
-            return res.status(203).json({ success: true, data: {}, message: "Tài khoản này không còn tồn tại" });
-        } catch (error) {
-            return res.status(500).json({ success: false, data: {}, message: "Lỗi: " + error.message });
-        }
-    }
-}
+    //   }
+    // }
+  }
