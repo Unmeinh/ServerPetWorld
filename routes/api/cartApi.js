73048143b1666dd09a72304eaa1e98caa1dd7c1f
ThanhBV <thanhbv@newwave.com.vn>
2023-09-28 21:57:35 +0700

    cart  locations

--- routes/api/cartApi.js
+++ routes/api/cartApi.js
@@ -5,7 +5,11 @@ var router = express.Router();
 
 
 router.get('/',mdJWT.api_auth, CartApiCtrl.cartUser);
+router.post('/checkProduct/:id',mdJWT.api_auth, CartApiCtrl.checkProduct)
 router.post('/',mdJWT.api_auth, CartApiCtrl.cartUser);
-router.post('/edit',mdJWT.api_auth, CartApiCtrl.editCart);
-
+router.post('/update',mdJWT.api_auth, CartApiCtrl.editCart);
+router.post('/updateLocations',mdJWT.api_auth, CartApiCtrl.updateLocaions)
+router.post('/addLocations',mdJWT.api_auth, CartApiCtrl.addLocations)
+router.put('/editLocations/:id',mdJWT.api_auth, CartApiCtrl.editLocationSelect)
+router.put('/editLocation',mdJWT.api_auth, CartApiCtrl.editLocation)
 module.exports = router;
