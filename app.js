72dd69562db05fae31070da89e8a8a8942edb04a
hoanglv03 <luonghoang29022003@gmail.com>
2023-10-09 23:51:46 +0700

    upgrade db

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var favoriteProductsRouter=require('./routes/api/myFavoriteProductApi');
 var billProductRouter = require('./routes/api/billProductApi');
 var voucherShopRouter = require('./routes/api/voucherShopApi');
 var voucherServerRouter = require('./routes/api/voucherServerApi');
-// var appointmentRouter = require('./routes/api/appointmentApi');
+var appointmentRouter = require('./routes/api/appointmentApi');
 var ConversationsRouter = require('./routes/api/ConversationsApi');
 var app = express();
 
@@ -77,7 +77,7 @@ app.use('/api/bill-product', billProductRouter);
 app.use('/api/favorite',favoriteProductsRouter);
 app.use('/api/voucherShop',voucherShopRouter);
 app.use('/api/voucherServer',voucherServerRouter);
-// app.use('/api/appointment',appointmentRouter);
+app.use('/api/appointment',appointmentRouter);
 app.use('/api/conversations', ConversationsRouter);
 
 // catch 404 and forward to error handler
