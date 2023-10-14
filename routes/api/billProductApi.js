938c5307d96b1d55f931a293c68b8d14c949115e
hoanglv03 <luonghoang29022003@gmail.com>
2023-10-14 15:18:25 +0700

    update bill

--- routes/api/billProductApi.js
+++ routes/api/billProductApi.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/listBillProduct',mdJWT.api_auth, billProductApiCtrl.listbillProduct);
 router.get('/detailBillProduct/:idBillPr',mdJWT.api_auth, billProductApiCtrl.detailBillProduct);
 router.post('/insert',mdJWT.api_auth, billProductApiCtrl.billProductUser);
+router.post('/cancelBill/:id',mdJWT.api_auth, billProductApiCtrl.cancelBill);
 //router.post('/edit',mdJWT.api_auth, billProductApiCtrl.editCart);
 
 module.exports = router;
