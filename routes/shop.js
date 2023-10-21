d47d5d35ab1c17fadd26cab6d537cd97e53e1283
sinka <sinka@dg-system.jp>
2023-10-21 12:12:53 +0700

    Update server 21/10/2023

--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,15 @@ var express = require('express');
 var ShopCtrl = require('../controller/shop.controller');
 var multer = require('multer');
 var uploader = multer({ dest: './tmp' });
+var checkLogin = require('../middlewares/checkLogin');
 var router = express.Router();
 
-router.get('/', ShopCtrl.listShop);
+router.get('/',checkLogin.check_request_login, ShopCtrl.listShop);
 
-router.get('/detail/:idShop', ShopCtrl.detailShop);//bổ sung /:idUser khi làm 
-router.get('/detailOwner/:idShop', ShopCtrl.detailOwner);//bổ sung /:idUser khi làm 
+router.get('/detail/:idShop',checkLogin.check_request_login, ShopCtrl.detailShop);//bổ sung /:idUser khi làm 
+router.get('/detailOwner/:idShop',checkLogin.check_request_login, ShopCtrl.detailOwner);//bổ sung /:idUser khi làm 
 
-router.get('/delete/:idShop',ShopCtrl.deleteShop);
-router.post('/delete/:idShop',ShopCtrl.deleteShop);
+router.get('/delete/:idShop',checkLogin.check_request_login,ShopCtrl.deleteShop);
+router.post('/delete/:idShop',checkLogin.check_request_login,ShopCtrl.deleteShop);
 
 module.exports = router;
