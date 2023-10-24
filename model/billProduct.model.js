d5fad990158a69cf1315084859ce368c350740e4
hoanglv03 <luonghoang29022003@gmail.com>
2023-10-24 20:59:42 +0700

    hot fix

--- model/billProduct.model.js
+++ model/billProduct.model.js
@@ -5,7 +5,7 @@ let billProductSchema = new db.mongoose.Schema(
     idUser: { type: db.mongoose.Schema.Types.ObjectId, ref: "UserModel" },
     idShop: { type: db.mongoose.Schema.Types.ObjectId, ref: "ShopModel" },
     locationDetail: {
-      name: { type: String, required: true },
+      fullName: { type: String, required: true },
       phoneNumber: { type: Number, required: true },
       location: { type: String, required: true },
     },
