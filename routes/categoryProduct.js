05d2021b8c2be177136d88f654ce1597937388ad
sinka <sinka@dg-system.jp>
2023-11-07 22:34:00 +0700

    [Component] fix layout screen

--- routes/categoryProduct.js
+++ routes/categoryProduct.js
@@ -1,19 +1,20 @@
 var express = require('express');
+var checkLoginServer = require('../middlewares/checkLogin');
 var categoryProductCtrl = require('../controller/categoryProduct.controller');
 
 var router = express.Router();
 
-router.get('/', categoryProductCtrl.listCategoryProduct);
+router.get('/',checkLoginServer.check_request_login, categoryProductCtrl.listCategoryProduct);
 
 // router.get('/detail', categoryProductCtrl.detailCategoryProduct);
 
-router.get('/add',categoryProductCtrl.addCategoryProduct);
+router.get('/add',checkLoginServer.check_request_login,categoryProductCtrl.addCategoryProduct);
 router.post('/add',categoryProductCtrl.addCategoryProduct);
 
-router.get('/edit/:idCat', categoryProductCtrl.editCategoryProduct);
+router.get('/edit/:idCat',checkLoginServer.check_request_login, categoryProductCtrl.editCategoryProduct);
 router.post('/edit/:idCat', categoryProductCtrl.editCategoryProduct);
 
-router.get('/delete/:idCat',categoryProductCtrl.deleteCategoryProduct);
+router.get('/delete/:idCat',checkLoginServer.check_request_login,categoryProductCtrl.deleteCategoryProduct);
 router.post('/delete/:idCat',categoryProductCtrl.deleteCategoryProduct);
 
 module.exports = router;
